1d5da0d1ce7cdd35e696799998b340e4ea9557fd
Pushpak_Doodleblue <pushpak@doodleblue.com>
2021-01-29 15:00:22 +0530

    Initial Commit

---
+++ app/src/main/java/com/firebase/firebasefacebook/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.firebase.firebasefacebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.firebase.firebasefacebook.databinding.ActivityMainBinding
+import java.util.zip.Inflater
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
